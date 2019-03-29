a8138b87f4f866464a10baed690853214d48811e
y-tsuzaki <>
2019-03-29 23:19:51 +0900

    リファクタリング漏れコミット

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Requests\PostRequest;
+use App\Http\Requests\PostAddRequest;
 use App\Thread; 
 use App\Post; 
 use DB; 
